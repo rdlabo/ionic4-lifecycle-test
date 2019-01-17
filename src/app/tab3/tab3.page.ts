90d3e01a4301998968e14083911cd7344db5f2a7
榊原昌彦 <sakakibara@rdlabo.jp>
2019-01-17 10:30:55 +0900

    add alert in lifecycle

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page implements OnInit {
+  ngOnInit () {
+    alert ('from Tab3Page ngOnInit');
+  }
+
+  ionViewWillEnter() {
+    alert ('from Tab3Page ionViewWillEnter');
+  }
+
+  ionViewDidEnter() {
+    alert ('from Tab3Page ionViewDidEnter');
+  }
+
+  ionViewWillLeave() {
+    alert ('from Tab3Page ionViewWillLeave');
+  }
+
+  ionViewDidLeave() {
+    alert ('from Tab3Page ionViewDidLeave');
+  }
+}
